606b7d80007c2bb02de36199c06d23bdc416e8e1
Naratna <antar.espadas@hotmail.com>
2022-10-03 19:25:03 -0500

    Improved layout

--- src/lib/grimoire-sheet/MainPage/Equipment.svelte
+++ src/lib/grimoire-sheet/MainPage/Equipment.svelte
@@ -6,6 +6,7 @@
 	import { types, typesi18n } from '$lib/values';
 	import ChippedDiv from '$lib/ChippedDiv.svelte';
 	import ChippedSelect from '$lib/ChippedSelect.svelte';
+	import LabeledInput from '$lib/LabeledInput.svelte';
 
 	const rollTemplate = `&{template:general} {{title=@{weapon_name}}} {{subtitle=@{weapon_damage_type} damage}} {{color=blue}} {{roll=[[@{weapon_damage}]]}}`;
 	const chatTemplate = `&{template:general} {{weapon=@{weapon_name}}} {{damage=@{weapon_damage_display}}} {{reach=@{weapon_reach}}} {{type=@{weapon_damage_type}}} {{effect=@{weapon_effect}}}`;
@@ -13,109 +14,94 @@
 	const t = types.map((type, i) => [type, typesi18n[i]]);
 </script>
 
-<Container>
+<div class="equipment">
 	<EditableField
 		fieldName="main_weapon"
 		hasRoll
 		rollAttribute="attr_weapon_attack"
+		display="grid"
 		{rollTemplate}
 		{chatTemplate}
 	>
-		<Row dense>
-			<Col class="col-4 pb-0">
-				<Label i18n="weapon">Weapon</Label>
-			</Col>
-			<Col class="col-2 pb-0">
-				<Label i18n="damage">Damage</Label>
-			</Col>
-			<Col class="col-2 pb-0">
-				<Label i18n="reach-range">Reach</Label>
-			</Col>
-			<Col class="col-2 pb-0">
-				<Label i18n="effect">Effect</Label>
-			</Col>
-		</Row>
+		<div class="always">
+			<Label i18n="weapon">Weapon</Label>
+			<Label i18n="damage">Damage</Label>
+			<Label i18n="reach-range">Reach</Label>
+			<Label i18n="effect">Effect</Label>
+		</div>
+
 		<svelte:fragment slot="edit">
-			<Row dense>
-				<Col class="col-4 pt-0">
-					<ChippedTextField name="attr_weapon_name" chipSize={7} />
-				</Col>
-				<Col class="col-2 pt-0">
-					<ChippedTextField name="attr_weapon_damage" chipSize={7} />
-				</Col>
-				<Col class="col-2 pt-0">
-					<ChippedTextField name="attr_weapon_reach" chipSize={7} />
-				</Col>
-				<Col class="pt-0">
-					<ChippedTextField name="attr_weapon_effect" chipSize={7} />
-				</Col>
-			</Row>
-			<Row dense>
-				<Col class="col-4">
-					<Label i18n="damage-type">Damage type</Label>
-				</Col>
-			</Row>
-			<Row dense>
-				<Col class="col-4">
-					<ChippedSelect name="attr_weapon_damage_type">
-						{#each t as [type, i18n]}
-							<option data-i18n={i18n} value={type}>{type}</option>
-						{/each}
-					</ChippedSelect>
-				</Col>
-			</Row>
+			<ChippedTextField chipSize={7} name="attr_weapon_name" />
+			<ChippedTextField chipSize={7} name="attr_weapon_damage" />
+			<ChippedTextField chipSize={7} name="attr_weapon_reach" />
+			<ChippedTextField chipSize={7} name="attr_weapon_effect" />
+
+			<Label i18n="damage-type" style="grid-row: 2;">Damage type</Label>
+
+			<ChippedSelect name="attr_weapon_damage_type">
+				{#each t as [type, i18n]}
+					<option data-i18n={i18n} value={type}>{type}</option>
+				{/each}
+			</ChippedSelect>
 		</svelte:fragment>
-		<Row dense slot="normal">
-			<Col class="col-4 pt-0">
-				<ChippedTextField name="attr_weapon_name" chipSize={7} disabled i18n="weapon-name" />
-			</Col>
-			<Col class="col-2 pt-0">
-				<ChippedTextField name="attr_weapon_damage_display" chipSize={7} disabled />
-			</Col>
-			<Col class="col-2 pt-0">
-				<ChippedTextField name="attr_weapon_reach" chipSize={7} disabled />
-			</Col>
-			<Col class="pt-0">
-				<ChippedTextField name="attr_weapon_effect" chipSize={7} disabled />
-			</Col>
-		</Row>
+
+		<svelte:fragment slot="normal">
+			<ChippedTextField name="attr_weapon_name" chipSize={7} disabled i18n="weapon-name" />
+			<ChippedTextField name="attr_weapon_damage_display" chipSize={7} disabled />
+			<ChippedTextField name="attr_weapon_reach" chipSize={7} disabled />
+			<ChippedTextField name="attr_weapon_effect" chipSize={7} disabled />
+		</svelte:fragment>
 	</EditableField>
-	<Row dense>
-		<Col class="col-4 pb-0">
-			<Label i18n="armor">Armor</Label>
-		</Col>
-		<Col class="col-2 pb-0">
-			<Label i18n="damage-reduction-short-short">Dmg Red</Label>
-		</Col>
-		<Col class="pb-0">
-			<Label i18n="effect">Effect</Label>
-		</Col>
-	</Row>
-	<Row dense>
-		<Col class="col-4 pt-0">
-			<ChippedTextField name="attr_armor_name" chipSize={7} />
-		</Col>
-		<Col class="col-2 pt-0">
-			<ChippedTextField name="attr_armor_damage_reduction" chipSize={7} />
-		</Col>
-		<Col class="pt-0">
-			<ChippedTextField name="attr_armor_effect" chipSize={7} />
-		</Col>
-	</Row>
-	<Row dense>
-		<Col class="col-4 pb-0">
-			<Label i18n="accessory">Accessory</Label>
-		</Col>
-		<Col class="pb-0">
-			<Label i18n="effect">Effect</Label>
-		</Col>
-	</Row>
-	<Row dense>
-		<Col class="col-4 pt-0">
-			<ChippedTextField name="attr_accessory_name" chipSize={7} />
-		</Col>
-		<Col class="pt-0">
-			<ChippedTextField name="attr_accessory_effect" chipSize={7} />
-		</Col>
-	</Row>
-</Container>
+
+	<LabeledInput i18n="armor" name="Armor" />
+	<LabeledInput i18n="damage-reduction-short-short" name="Dmg red" />
+	<LabeledInput i18n="effect" name="armor-effect" />
+
+	<LabeledInput i18n="accessory" name="Accessory" />
+	<LabeledInput i18n="effect" name="accessory-effect" />
+</div>
+
+<style lang="scss">
+	.equipment {
+		--cols: 4fr 2fr 2fr 4fr;
+		--gap: 5px;
+
+		padding: 10px;
+		display: grid;
+		gap: var(--gap);
+		grid-template-columns: var(--cols);
+		grid-template-areas:
+			'weapon weapon weapon weapon'
+			'damage-type-label . . .'
+			'damage-type . . .'
+			'armor-label dmg-red-label armor-effect-label armor-effect-label'
+			'armor dmg-red armor-effect armor-effect'
+			'accessory-label accessory-effect-label accessory-effect-label accessory-effect-label'
+			'accessory accessory-effect accessory-effect accessory-effect';
+
+		:global(.editable-field) {
+			grid-area: weapon;
+		}
+
+		:global(.editable-field .edit) {
+			gap: var(--gap);
+			grid-template-columns: var(--cols);
+			grid-template-rows: repeat(3, auto);
+
+			:global(.chipped-select) {
+				grid-row: 3;
+			}
+		}
+
+		:global(.editable-field .normal) {
+			gap: var(--gap);
+			grid-template-columns: var(--cols);
+		}
+
+		.always {
+			display: grid;
+			gap: var(--gap);
+			grid-template-columns: var(--cols);
+		}
+	}
+</style>
