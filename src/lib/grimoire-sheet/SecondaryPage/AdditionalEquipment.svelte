19e73d1e4f23ddacee2ff305faddd125beaba861
Naratna <antar.espadas@hotmail.com>
2022-10-20 22:18:57 -0500

    Improved AdditionalEquipment layout

--- src/lib/grimoire-sheet/SecondaryPage/AdditionalEquipment.svelte
+++ src/lib/grimoire-sheet/SecondaryPage/AdditionalEquipment.svelte
@@ -1,46 +1,26 @@
 <script lang="ts">
-	import ChippedTextField from '$lib/ChippedTextField.svelte';
-	import Label from '$lib/Label.svelte';
-	import { Container, Row, Col } from 'svelte-materialify';
+	import LabeledInput from '$lib/LabeledInput.svelte';
 </script>
 
-<Container class="pt-0">
-	<Row dense>
-		<Col class="col-5">
-			<Label i18n="sidearm">Sidearm</Label>
-		</Col>
-		<Col class="col-2">
-			<Label i18n="damage">Damage</Label>
-		</Col>
-		<Col class="col-2">
-			<Label i18n="reach-range">Reach</Label>
-		</Col>
-		<Col class="col-3">
-			<Label i18n="effect">Effect</Label>
-		</Col>
-	</Row>
-	<Row dense>
-		<Col class="col-5">
-			<ChippedTextField name="attr_sidearm_name" chipSize={7} />
-		</Col>
-		<Col class="col-2">
-			<ChippedTextField name="attr_sidearm_damage" chipSize={7} />
-		</Col>
-		<Col class="col-2">
-			<ChippedTextField name="attr_sidearm_reach" chipSize={7} />
-		</Col>
-		<Col class="col-3">
-			<ChippedTextField name="attr_sidearm_effect" chipSize={7} />
-		</Col>
-	</Row>
-	<Row dense>
-		<Col>
-			<Label i18n="miscellaneous-gear">Misc Gear</Label>
-		</Col>
-	</Row>
-	<Row dense>
-		<Col>
-			<ChippedTextField name="attr_misc_gear" chipSize={7} />
-		</Col>
-	</Row>
-</Container>
+<div class="additional-equipment">
+	<LabeledInput i18n="sidearm" name="Sidearm" />
+	<LabeledInput i18n="damage" name="Damage" />
+	<LabeledInput i18n="reach" name="Reach" />
+	<LabeledInput i18n="effect" name="Effect" />
+
+	<LabeledInput i18n="miscellaneous-gear" name="Misc Gear" />
+</div>
+
+<style lang="scss">
+	.additional-equipment {
+		padding: 10px;
+		display: grid;
+		gap: 5px;
+		grid-template-columns: 5fr 2fr 2fr 3fr;
+		grid-template-areas:
+			'sidearm-label damage-label reach-label effect-label'
+			'sidearm damage reach effect'
+			'misc-gear-label misc-gear-label misc-gear-label misc-gear-label'
+			'misc-gear misc-gear misc-gear misc-gear';
+	}
+</style>
