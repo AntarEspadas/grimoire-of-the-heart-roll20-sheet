91db0b144e522f32887d547f66b2feb191e2c1f3
Naratna <antar.espadas@hotmail.com>
2022-10-22 03:29:05 -0500

    Improved SecondaryPage layout

--- src/lib/grimoire-sheet/SecondaryPage/SecondaryPage.svelte
+++ src/lib/grimoire-sheet/SecondaryPage/SecondaryPage.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import ChippedDiv from '$lib/ChippedDiv.svelte';
-	import { Container, Row, Col } from 'svelte-materialify';
 	import AdditionalEquipment from './AdditionalEquipment.svelte';
 	import Confidants from './Confidants.svelte';
 	import Feats from './Feats.svelte';
@@ -9,59 +8,59 @@
 	import Notes from './Notes.svelte';
 	import Persona from './Persona.svelte';
 	import Portrait from './Portrait.svelte';
-
-	let bgColor = '#8182AB';
 </script>
 
-<Container fluid>
-	<Row>
-		<Col class="col-3 d-none d-sm-block">
-			<ChippedDiv chipSize={40} w="100%" h="225px" {bgColor}>
-				<Portrait />
-			</ChippedDiv>
-		</Col>
-		<Col>
-			<ChippedDiv chipSize={30} w="100%" h="225px" {bgColor}>
-				<Header />
-			</ChippedDiv>
-		</Col>
-	</Row>
-	<Row>
-		<Col class="col-12 sm-7">
-			<Row dense class="mb-4">
-				<ChippedDiv chipSize={25} w="100%" h="150px" {bgColor}>
-					<AdditionalEquipment />
-				</ChippedDiv>
-			</Row>
-			<Row dense>
-				<ChippedDiv chipSize={40} w="100%" style="min-height: 335px;" {bgColor}>
-					<Persona />
-				</ChippedDiv>
-			</Row>
-		</Col>
-		<Col>
-			<ChippedDiv chipSize={40} w="100%" style="min-height: 500px;" {bgColor}>
-				<Feats />
-			</ChippedDiv>
-		</Col>
-	</Row>
-	<Row>
-		<Col class="col-12 sm-7">
-			<Row dense class="mb-4">
-				<ChippedDiv chipSize={40} w="100%" style="min-height: 400px;" {bgColor}>
-					<Items />
-				</ChippedDiv>
-			</Row>
-			<Row dense>
-				<ChippedDiv chipSize={40} w="100%" h="307px" {bgColor}>
-					<Notes />
-				</ChippedDiv>
-			</Row>
-		</Col>
-		<Col>
-			<ChippedDiv chipSize={40} w="100%" h="725px" {bgColor}>
-				<Confidants />
-			</ChippedDiv>
-		</Col>
-	</Row>
-</Container>
+<div class="secondary-page">
+	<ChippedDiv chipSize={40} style="grid-area: portrait;">
+		<Portrait />
+	</ChippedDiv>
+	<ChippedDiv chipSize={30} style="grid-area: header;">
+		<Header />
+	</ChippedDiv>
+	<ChippedDiv chipSize={25} style="grid-area: additional-equipment;">
+		<AdditionalEquipment />
+	</ChippedDiv>
+	<ChippedDiv chipSize={40} style="grid-area: persona;">
+		<Persona />
+	</ChippedDiv>
+	<ChippedDiv chipSize={40} style="grid-area: feats;">
+		<Feats />
+	</ChippedDiv>
+	<ChippedDiv chipSize={40} style="grid-area: items;">
+		<Items />
+	</ChippedDiv>
+	<ChippedDiv chipSize={40} style="grid-area: notes;">
+		<Notes />
+	</ChippedDiv>
+	<ChippedDiv chipSize={40} style="grid-area: confidants;">
+		<Confidants />
+	</ChippedDiv>
+</div>
+
+<style lang="scss">
+	.secondary-page {
+		padding: 10px;
+		display: grid;
+		gap: 24px;
+		grid-template-rows: 0px 225px 150px minmax(150px, auto) minmax(150px, auto) minmax(150px, auto) auto 300px;
+		grid-template-areas: 'portrait' 'header' 'additional-equipment' 'persona' 'feats' 'items' 'confidants' 'notes';
+	}
+
+	@media (min-width: 600px) {
+		.secondary-page {
+			grid-template-columns: 2fr 3fr 3fr;
+			grid-template-rows: 225px 150px minmax(335px, auto) minmax(400px, auto) 300px;
+			grid-template-areas:
+				'portrait header header'
+				'additional-equipment additional-equipment feats'
+				'persona persona feats'
+				'items items confidants'
+				'notes notes confidants';
+		}
+	}
+
+	.secondary-page > :global(.chipped-div) {
+		width: 100%;
+		background-color: #8182ab;
+	}
+</style>
