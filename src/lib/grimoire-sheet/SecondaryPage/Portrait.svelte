ecb35b3aeaa371a447db62ed7995f35f56171a49
Naratna <antar.espadas@hotmail.com>
2022-10-22 02:29:10 -0500

    Improved Portrait layout

--- src/lib/grimoire-sheet/SecondaryPage/Portrait.svelte
+++ src/lib/grimoire-sheet/SecondaryPage/Portrait.svelte
@@ -1,28 +1,24 @@
 <script lang="ts">
-	import { Container, Row, Col } from 'svelte-materialify';
 	import ChippedDiv from '$lib/ChippedDiv.svelte';
 	import Label from '$lib/Label.svelte';
 </script>
 
-<Container class="pt-0 pr-2 pl-2">
-	<Row dense>
-		<Col class="pb-0">
-			<Label i18n="portrait">Portrait</Label>
-		</Col>
-	</Row>
-	<Row dense>
-		<Col class="pt-0">
-			<ChippedDiv chipSize={30} w="100%" h="190px" bgColor="white">
-				<img alt="avatar" name="attr_character_avatar" />
-			</ChippedDiv>
-		</Col>
-	</Row>
-</Container>
->
+<div class="portrait">
+	<Label i18n="portrait">Portrait</Label>
+	<ChippedDiv chipSize={30} bgColor="white">
+		<img alt="avatar" name="attr_character_avatar" />
+	</ChippedDiv>
+</div>
 
 <style lang="sass">
     img
         width: 100%
         height: 100%
         object-fit: cover
+
+    .portrait
+        height: 100%
+        padding: 8px
+        display: grid
+        grid-template-rows: auto 1fr
 </style>
