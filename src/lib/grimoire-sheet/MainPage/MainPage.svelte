2e386b72e5eb7f4d5caec87771bb69d3cfa8649b
Naratna <antar.espadas@hotmail.com>
2022-10-22 04:18:47 -0500

    Improved MainPage layout

--- src/lib/grimoire-sheet/MainPage/MainPage.svelte
+++ src/lib/grimoire-sheet/MainPage/MainPage.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { Row, Col, Container } from 'svelte-materialify';
 	import ChippedDiv, { Corner } from '$lib/ChippedDiv.svelte';
 	import Header from './Header.svelte';
 	import CombatSkills from './CombatSkills.svelte';
@@ -11,124 +10,140 @@
 	import Equipment from './Equipment.svelte';
 	import Resistances from './Resistances.svelte';
 	import CombatRolls from './CombatRolls.svelte';
-
-	const debug = false;
-
-	let bgColor = debug ? 'rgba(255, 149, 174, 0.5)' : '#8395AE';
 </script>
 
-<div class="guide" style={debug ? undefined : 'background-image: unset;'}>
-	<Container fluid>
-		<Row>
-			<Col class="d-none d-sm-flex">
-				<img
-					src="https://raw.githubusercontent.com/Naratna/grimoire-of-the-heart-roll20-sheet/master/static/assets/grimoire-logo.png"
-					alt="logo"
-				/>
-			</Col>
-			<Col class="col-12 sm-9 pl-sm-0">
-				<ChippedDiv w="100%" smH="125px" h="175px" {bgColor} chipSize={20}>
-					<Header />
-				</ChippedDiv>
-			</Col>
-		</Row>
-		<Row>
-			<Col>
-				<ChippedDiv w="100%" {bgColor} chipSize={20}>
-					<CombatRolls initiative />
-				</ChippedDiv>
-			</Col>
-		</Row>
-		<Row>
-			<Col class="col-12 sm-6">
-				<ChippedDiv corner={Corner.bottomLeft} w="100%" h="250px" {bgColor} chipSize={40}>
-					<CombatSkills />
-				</ChippedDiv>
-			</Col>
-			<Col class="col-12 sm-6">
-				<ChippedDiv w="100%" h="250px" {bgColor} chipSize={40}>
-					<HpAndEnergy />
-				</ChippedDiv>
-			</Col>
-		</Row>
-		<Row>
-			<Col class="col-12 sm-5">
-				<ChippedDiv corner={Corner.bottomLeft} w="100%" h="300px" {bgColor} chipSize={40}>
-					<SocialSkills />
-				</ChippedDiv>
-			</Col>
-			<Col>
-				<ChippedDiv w="100%" h="300px" {bgColor} chipSize={40}>
-					<Aspects />
-				</ChippedDiv>
-			</Col>
-		</Row>
-		<Row>
-			<Col>
-				<div class="persona-and-equipment" style="background-color: {bgColor};">
-					<div class="persona">
-						<Persona />
-					</div>
-					<div class="types">
-						<Types />
-					</div>
-					<div class="equipment">
-						<Equipment />
-					</div>
-					<div class="resistances">
-						<Resistances />
-					</div>
-				</div>
-			</Col>
-		</Row>
-	</Container>
+<div class="main-page">
+	<img
+		src="https://raw.githubusercontent.com/Naratna/grimoire-of-the-heart-roll20-sheet/master/static/assets/grimoire-logo.png"
+		alt="logo"
+	/>
+	<ChippedDiv chipSize={20} style="grid-area: header;">
+		<Header />
+	</ChippedDiv>
+	<ChippedDiv chipSize={20} style="grid-area: rolls;">
+		<CombatRolls initiative />
+	</ChippedDiv>
+	<ChippedDiv corner={Corner.bottomLeft} chipSize={40} style="grid-area: combat-skills;">
+		<CombatSkills />
+	</ChippedDiv>
+	<ChippedDiv chipSize={40} style="grid-area: hp;">
+		<HpAndEnergy />
+	</ChippedDiv>
+	<ChippedDiv corner={Corner.bottomLeft} chipSize={40} style="grid-area: social-skills;">
+		<SocialSkills />
+	</ChippedDiv>
+	<ChippedDiv chipSize={40} style="grid-area: aspects;">
+		<Aspects />
+	</ChippedDiv>
+	<div class="persona-and-equipment">
+		<div class="persona">
+			<Persona />
+		</div>
+		<div class="types">
+			<Types />
+		</div>
+		<div class="equipment">
+			<Equipment />
+		</div>
+		<div class="resistances">
+			<Resistances />
+		</div>
+	</div>
 </div>
 
-<style lang="sass">
+<style lang="scss">
+	.main-page {
+		padding: 10px;
+		display: grid;
+		gap: 24px;
+		grid-template-rows: 0px repeat(5, auto) 300px;
+		grid-template-areas: 'img' 'header' 'rolls' 'combat-skills' 'hp' 'social-skills' 'aspects' 'persona';
+	}
 
-	img
-		width: 100%
-		object-fit: contain
+	img {
+		display: none;
+	}
 
-	.guide
-		background: url("https://raw.githubusercontent.com/grimoireofheart/grimoireofheart.github.io/main/Resources/Sheets/PNG/Character%20Sheet/Character%20Sheet-1.png")
-		height: 100%
-		background-size: 107%
-		background-position: center center
+	@media (min-width: 600px) {
+		.main-page {
+			grid-template-columns: 3fr 2fr 1fr 6fr;
+			grid-template-rows: 125px;
+			grid-template-areas:
+				'img header header header'
+				'rolls rolls rolls rolls'
+				'combat-skills combat-skills combat-skills hp'
+				'social-skills social-skills aspects aspects'
+				'persona persona persona persona';
+		}
 
-	.persona-and-equipment	
-		--down: 45%
-		--chip-size: 40px
-		width: 100%
-		height: 500px
-		clip-path: polygon(100% var(--chip-size), calc(100% - var(--chip-size)) 0, 0 0, 0 var(--down), 62.5% var(--down), 62.5% 100%, 61% 100%, 61% calc(var(--down) + 4%), 0 calc( var(--down) + 4% ), 0 calc(100% - var(--chip-size)), var(--chip-size) 100%, 100% 100%)
-		display: grid
-		grid-template-columns: 61% 1fr
-		grid-template-rows: 25% calc(var(--down) - 25%) 1fr
-		column-gap: 1.5%
-		row-gap: 2%
+		img {
+			height: 100%;
+			object-fit: contain;
+			grid-area: img;
+			display: block;
+			margin-right: auto;
+			margin-left: auto;
+		}
+	}
 
-	.persona
-		width: 100%
-		height: 100%
-		grid-row: 1 / 3
-		grid-column: 1
-	
-	.types
-		width: 100%
-		height: 100%
-		grid-row: 1
-		grid-column: 2
+	.main-page > :global(.chipped-div) {
+		width: 100%;
+		background-color: #8395ae;
+	}
 
-	.equipment
-		width: 100%
-		height: 100%
-		grid-row: 3
-		grid-column: 1
+	.persona-and-equipment {
+		--down: 45%;
+		--chip-size: 40px;
+		grid-area: persona;
+		background-color: #8395ae;
+		width: 100%;
+		height: 500px;
+		clip-path: polygon(
+			100% var(--chip-size),
+			calc(100% - var(--chip-size)) 0,
+			0 0,
+			0 var(--down),
+			62.5% var(--down),
+			62.5% 100%,
+			61% 100%,
+			61% calc(var(--down) + 4%),
+			0 calc(var(--down) + 4%),
+			0 calc(100% - var(--chip-size)),
+			var(--chip-size) 100%,
+			100% 100%
+		);
+		display: grid;
+		grid-template-columns: 61% 1fr;
+		grid-template-rows: 25% calc(var(--down) - 25%) 1fr;
+		column-gap: 1.5%;
+		row-gap: 2%;
+	}
 
-	.resistances
-		width: 100%
-		height: 100%
-		grid-row: 2 / 4
-		grid-column: 2
+	.persona {
+		width: 100%;
+		height: 100%;
+		grid-row: 1 / 3;
+		grid-column: 1;
+	}
+
+	.types {
+		width: 100%;
+		height: 100%;
+		grid-row: 1;
+		grid-column: 2;
+	}
+
+	.equipment {
+		width: 100%;
+		height: 100%;
+		grid-row: 3;
+		grid-column: 1;
+	}
+
+	.resistances {
+		width: 100%;
+		height: 100%;
+		grid-row: 2 / 4;
+		grid-column: 2;
+	}
 </style>
